eb0d7cf6ca36d939af84e95fe98aa71254a28781
mn <mn@mnui-MacBookPro.local>
2022-09-21 00:54:22 +0900

    module.export issue

--- akaps-sniping/vue.config.js
+++ akaps-sniping/vue.config.js
@@ -1,8 +1,8 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-module.exports = {
-  publicPath: "/akaps-sniping/",
-  outputDir: "docs",
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+// module.exports = {
+//   publicPath: "/akas-sniping/",
+//   outputDir: "docs",
+// };
